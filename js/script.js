463365b1c0e07cce91a63e9e14e0894aa9e1e397
gordela <giorgi.gordiashvili.1@btu.edu.ge>
2020-07-20 03:34:02 +0400

    news and slider news

--- js/script.js
+++ js/script.js
@@ -6,3 +6,22 @@ burger.addEventListener("click", () => {
   nav.classList.toggle("nav-active");
   content.classList.toggle("content-active");
 });
+var swiper = new Swiper(".swiper-container", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  // init: false,
+  navigation: {
+    nextEl: ".slider-next",
+    prevEl: ".slider-prev",
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
+  },
+});
