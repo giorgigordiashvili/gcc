6eaf01aa84a5da8b804fe619d96795ef83d0a1dd
gordela <giorgi.gordiashvili.1@btu.edu.ge>
2020-07-26 18:11:03 +0400

    all apges

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ const nav = document.querySelector(".nav-links");
 const content = document.querySelector(".content");
 burger.addEventListener("click", () => {
   nav.classList.toggle("nav-active");
-  content.classList.toggle("content-active");
 });
 
 const navSearch = document.querySelector(".search-button");
@@ -14,6 +13,16 @@ navSearch.addEventListener("click", () => {
   navInput.classList.toggle("search-active");
 });
 
+function dropdown() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+function closeModal(id) {
+  document.getElementById("myModal" + id).style.display = "none";
+}
+function openModal(id) {
+  document.getElementById("myModal" + id).style.display = "block";
+}
+
 var swiper = new Swiper(".swiper-container", {
   slidesPerView: 1,
   spaceBetween: 10,
@@ -22,6 +31,7 @@ var swiper = new Swiper(".swiper-container", {
     nextEl: ".slider-next",
     prevEl: ".slider-prev",
   },
+
   breakpoints: {
     768: {
       slidesPerView: 1,
@@ -36,7 +46,7 @@ var swiper = new Swiper(".swiper-container", {
 
 var slider = new Swiper(".home-slider-container", {
   direction: document.body.clientWidth < 768 ? "horizontal" : "vertical",
-
+  parallax: true,
   loop: true,
   grabCursor: true,
   autoplay: {
@@ -51,6 +61,40 @@ var slider = new Swiper(".home-slider-container", {
 });
 
 var swiper = new Swiper(".team-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  // init: false,
+
+  navigation: {
+    nextEl: ".team-button-next",
+    prevEl: ".team-button-prev",
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
+  },
+});
+
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
+
+var swiper = new Swiper(".team-swiper-container", {
   slidesPerView: 1,
   spaceBetween: 10,
   // init: false,
